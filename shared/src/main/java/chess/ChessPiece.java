3aef8b847e6bd149cf37a4193d8f6e5bd8721de3
DanielTyra <danieltyra123@gmail.com>
2026-01-24 23:03:59 -0700

    impl(p0) basic move functionality

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,7 +1,9 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -11,7 +13,12 @@ import java.util.List;
  */
 public class ChessPiece {
 
+    private final ChessGame.TeamColor pieceColor;
+    private final PieceType type;
+
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +37,28 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece piece = (ChessPiece) o;
+        return pieceColor == piece.pieceColor && type == piece.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
     }
 
     /**
@@ -48,6 +69,33 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return List.of();
+        ChessPiece piece = board.getPiece(myPosition);
+        Collection<ChessMove> moves = new ArrayList<>();
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            int[][] directions = {{1,1}, {1,-1}, {-1,1}, {-1,-1}};
+            for (int[]direction: directions) {
+                int r = myPosition.getRow() + direction[0];
+                int c = myPosition.getColumn() + direction[1];
+                while (r >= 1 && r <= 8 && c >= 1 && c <=8) {
+                    ChessPosition targetPosition = new ChessPosition(r, c);
+                    ChessPiece targetPiece = board.getPiece(targetPosition);
+                    if (targetPiece == null) {
+                        moves.add(new ChessMove(myPosition, targetPosition, null));
+                    } else if (targetPiece.getTeamColor() != pieceColor) {
+                        moves.add(new ChessMove(myPosition, targetPosition, null));
+                        break;
+                    } else if (targetPiece.getTeamColor() == pieceColor) {
+                        break;
+                    }
+                    r+=direction[0];
+                    c+=direction[1];
+                }
+            }
+
+
+        }
+
+
+        return moves;
     }
 }
