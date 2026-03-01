7ef14a882c4c4b2bd4f77212f5ed0cd60e6ce6b3
DanielTyra <danieltyra123@gmail.com>
2026-03-01 15:56:57 -0700

    Added UserService for register, login, and logout

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,56 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.*;
+
+import java.util.UUID;
+
+public class UserService {
+
+    private final DataAccess dao;
+
+    public UserService(DataAccess dao) {
+        this.dao = dao;
+    }
+
+    public AuthData register(UserData user) throws DataAccessException {
+        if (user.username() == null || user.password() == null || user.email() == null) {
+            throw new DataAccessException("bad request");
+        }
+
+        if (dao.getUser(user.username()) != null) {
+            throw new DataAccessException("already taken");
+        }
+
+        dao.createUser(user);
+
+        String token = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(token, user.username());
+        dao.createAuth(auth);
+
+        return auth;
+    }
+
+    public AuthData login(String username, String password) throws DataAccessException {
+        UserData user = dao.getUser(username);
+
+        if (user == null || !user.password().equals(password)) {
+            throw new DataAccessException("unauthorized");
+        }
+
+        String token = UUID.randomUUID().toString();
+        AuthData auth = new AuthData(token, username);
+        dao.createAuth(auth);
+
+        return auth;
+    }
+
+    public void logout(String authToken) throws DataAccessException {
+        if (dao.getAuth(authToken) == null) {
+            throw new DataAccessException("unauthorized");
+        }
+
+        dao.deleteAuth(authToken);
+    }
+}
