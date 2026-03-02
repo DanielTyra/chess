8daf5ae04760dfbe53ed9b820a4d4592a9d850fd
DanielTyra <danieltyra123@gmail.com>
2026-03-01 19:04:24 -0700

    Added UGameService

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,76 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.*;
+
+import java.util.List;
+
+public class GameService {
+
+    private final DataAccess dao;
+
+    public GameService(DataAccess dao) {
+        this.dao = dao;
+    }
+
+    public List<GameData> listGames(String authToken) throws DataAccessException {
+        if (dao.getAuth(authToken) == null) {
+            throw new DataAccessException("unauthorized");
+        }
+
+        return dao.listGames();
+    }
+
+    public int createGame(String authToken, String gameName) throws DataAccessException {
+        if (dao.getAuth(authToken) == null) {
+            throw new DataAccessException("unauthorized");
+        }
+
+        if (gameName == null) {
+            throw new DataAccessException("bad request");
+        }
+
+        return dao.createGame(gameName);
+    }
+
+    public void joinGame(String authToken, int gameID, String playerColor) throws DataAccessException {
+        AuthData auth = dao.getAuth(authToken);
+        if (auth == null) {
+            throw new DataAccessException("unauthorized");
+        }
+
+        GameData game = dao.getGame(gameID);
+        if (game == null) {
+            throw new DataAccessException("bad request");
+        }
+
+        if (playerColor.equals("WHITE")) {
+            if (game.whiteUsername() != null) {
+                throw new DataAccessException("already taken");
+            }
+
+            dao.updateGame(new GameData(
+                    gameID,
+                    auth.username(),
+                    game.blackUsername(),
+                    game.gameName(),
+                    game.game()
+            ));
+        } else if (playerColor.equals("BLACK")) {
+            if (game.blackUsername() != null) {
+                throw new DataAccessException("already taken");
+            }
+
+            dao.updateGame(new GameData(
+                    gameID,
+                    game.whiteUsername(),
+                    auth.username(),
+                    game.gameName(),
+                    game.game()
+            ));
+        } else {
+            throw new DataAccessException("bad request");
+        }
+    }
+}
